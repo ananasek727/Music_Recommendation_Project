a28e0745199e9b4736e0701751e7092d3caaff77
ananasek727 <piotr.mozeluk@gmail.com>
2023-12-11 20:45:38 +0100

    navBarPage.test.js

--- front-end/src/pages/__test__/navBarPage.test.js
+++ front-end/src/pages/__test__/navBarPage.test.js
@@ -1,37 +1,46 @@
-import {shallow} from 'enzyme';
-import {configure, simulate, text} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import NavBarPage from "../NavBarPage";
 import React from 'react';
-import {render, screen} from '@testing-library/react';
-configure({adapter: new Adapter()});
-import { BrowserRouter as Router } from 'react-router-dom';
-  it('renders Navbar with title "Mus4You"', () => {
-    render(
-      <Router>
-        <NavBarPage />
-      </Router>
-    );
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import NavBarPage from '../NavBarPage';
+import {BrowserRouter} from "react-router-dom";
 
-    // Assert that the title "Mus4You" is present in the rendered component
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({ url: 'test', data:'data'}),
+  })
+);
+
+describe('NavBarPage component', () => {
+  it('renders without crashing', () => {
+    render   (<BrowserRouter>
+        <NavBarPage isLoggedInSpotify={true} />
+      </BrowserRouter>);
     expect(screen.getByText('Mus4You')).toBeInTheDocument();
   });
 
-  it('does not render Home button when not on the /music route', () => {
-    // Mock useLocation to return a pathname other than '/music'
-    jest.mock('react-router-dom', () => ({
-      ...jest.requireActual('react-router-dom'),
-      useLocation: () => ({
-        pathname: '/some-other-route',
-      }),
-    }));
-
-    render(
-      <Router>
-        <NavBarPage />
-      </Router>
-    );
-
-    // Assert that the Home button is not rendered when not on the /music route
-    expect(screen.queryByText('Home')).toBeNull();
+  it('renders the "Logout" button when isLoggedInSpotify is true', () => {
+    render   (<BrowserRouter>
+        <NavBarPage isLoggedInSpotify={true} />
+      </BrowserRouter>);
+    expect(screen.getByText('Logout')).toBeInTheDocument();
   });
+
+  it('does not render the "Logout" button when isLoggedInSpotify is false', () => {
+        render   (<BrowserRouter>
+        <NavBarPage isLoggedInSpotify={false} />
+      </BrowserRouter>);
+    expect(screen.queryByText('Logout')).not.toBeInTheDocument();
+  });
+
+  it('calls the handleLogOut function when "Logout" button is clicked', async () => {
+    render   (<BrowserRouter>
+        <NavBarPage isLoggedInSpotify={true} />
+      </BrowserRouter>);
+    const logoutButton = screen.getByText('Logout');
+    fireEvent.click(logoutButton);
+    expect(global.fetch).toHaveBeenCalledWith('http://127.0.0.1:8000/logout', {
+      method: 'DELETE',
+    });
+  });
+});
