8f60ef51a25b3995e73e64db0c6890d212065e1e
justynapokora <justynapokora@icloud.com>
2024-01-27 17:32:41 +0100

    remove old datasets & old backend

--- music_recommendation/main_app/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ImageUpload
-
-
-class ImageUploadSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ImageUpload
-        fields = '__all__'
